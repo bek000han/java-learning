d9e6a4779a52f66abccae4be0dd4f6e20d157ed2
Bekz <bekzatkhan.maulen@gmail.com>
2023-06-24 20:46:40 +0600

    2nd part done

---
+++ part2-functionality/Division.java
@@ -0,0 +1,20 @@
+
+
+public class Division {
+
+    public static void main(String[] args) {
+        
+        // Once you have implemented the division method, you can
+        // try it out here. Fopr example division(3,5);
+        // should print "0.6"
+
+        // division(3, 5);
+        division(3, 5);
+    }
+
+    // implement the method here
+    public static void division(int num, int den) {
+        double ans = 1.0 * num / den;
+        System.out.println(ans);
+    }
+}
