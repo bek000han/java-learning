58dd4f624e4e58807cc7c0f949f8655bac5cd49c
Bekz <bekzatkhan.maulen@gmail.com>
2023-07-12 21:38:32 +0600

    2nd part of part 5

---
+++ part5-oop2/HealthStation.java
@@ -0,0 +1,19 @@
+
+public class HealthStation {
+    private int weighings;
+
+    public int weigh(Person person) {
+        // return the weight of the person passed as the parameter
+        this.weighings++;
+        return person.getWeight();
+    }
+    
+    public void feed(Person person) {
+        person.setWeight(person.getWeight() + 1);
+    }
+    
+    public int weighings() {
+        return weighings;
+    }
+
+}
