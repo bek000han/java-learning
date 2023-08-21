409445b1148c42866e796aad164160e2d0da9253
Bekz <bekzatkhan.maulen@gmail.com>
2023-08-21 14:22:23 +0600

    first upload of part 1

---
+++ part11-class-diagrams/Bot.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author RM
+ */
+public class Bot extends Player {
+    
+    public void addMove(String move) {
+        
+    }
+}
