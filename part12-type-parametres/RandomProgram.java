cce6813865f4fc91d03daf58809a3656813465d3
Bekz <bekzatkhan.maulen@gmail.com>
2023-08-29 22:17:58 +0600

    Add files via upload

---
+++ part12-type-parametres/RandomProgram.java
@@ -0,0 +1,20 @@
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        
+        System.out.println("How many random numbers should be printed?");
+        int num = Integer.valueOf(scanner.nextLine());
+        
+        for (int i = 0; i < num; i++) {
+            int randomNumber = random.nextInt(11);
+            System.out.println(randomNumber);
+        }
+    }
+
+}
